e49cf7e6dcec307d2d5c71c9d304c74f848aedb2
Denis Patrikeev <patrikeev.denya@yandex.ru>
2024-10-16 13:58:38 +0300

    3 commit

--- src/main/java/api/models/User.java
+++ src/main/java/api/models/User.java
@@ -8,28 +8,12 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 public class User {
-
     private String email;
     private String name;
     private String password;
 
-    public User(String email, String password, String name) {
-    }
-
     @Override
     public String toString() {
         return "email: " + email + ", password: " + password + ", name: " + name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
     }
 }
